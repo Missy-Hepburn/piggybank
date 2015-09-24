e65fd483fb8083a9eba8b4615bba5c92b5459ad8
Missy-Hepburn <audreygiroud@hotmail.com>
2015-09-24 16:07:52 +0000

    sendgrid config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'pimpmypiggybank',
+  :password => 'Piggybank2014',
+  :domain => 'http://pimpmypiggybank.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
