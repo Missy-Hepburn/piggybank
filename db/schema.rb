82dba98c740866d54dd9ae1ddb62317c63ce23b9
Missy-Hepburn <audreygiroud@hotmail.com>
2015-11-12 13:34:52 +0000

    with confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917160948) do
+ActiveRecord::Schema.define(version: 20151111141036) do
 
   create_table "budgets", force: :cascade do |t|
     t.string   "savings_target"
@@ -128,8 +128,13 @@ ActiveRecord::Schema.define(version: 20150917160948) do
     t.integer  "interest_rate"
     t.float    "weekly_expenses"
     t.float    "monthly_expenses"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
